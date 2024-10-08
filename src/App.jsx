544775cb494b031947c65997d443115660d35405
lucaslmb971 <lucaslmb971@gmail.com>
2024-10-07 23:40:56 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+            <div className="top-nav"></div>
+            <div className="content">
+                <div className="main">
+                    <div className="sub-content"></div>
+                    <div className="sub-content"></div>
+                    <div className="sub-content"></div>
+                    <div id="advertisement"></div>
+                </div>
+                <div className="side-nav"></div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default App
